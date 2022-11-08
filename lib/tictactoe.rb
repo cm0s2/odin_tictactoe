a7ed56ed6d4f85fd6dd477f3d896a9e1bbb86ea3
Carl Gudmundsson <komoss@gmail.com>
2022-11-08 22:47:48 +0100

    Refactor TicTacToe to make it easier to test

--- lib/tictactoe.rb
+++ lib/tictactoe.rb
@@ -5,37 +5,38 @@ class TicTacToe
   LINES = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9],
            [1, 5, 9], [7, 5, 3]].freeze
 
-  def initialize(player_class1, player_class2)
-    @grid = Array.new(10)
-    @players = [player_class1.new(self, 'X'), player_class2.new(self, 'O')]
+  def initialize(player_class1: Player, player_class2: Player, grid: Array.new(10))
+    @grid = grid
+    @player1 = player_class1.new(self, 'X')
+    @player2 = player_class2.new(self, 'O')
+    @current_player = @player1
   end
 
   def play
     print_board
-    until game_over?
-      n = n == 0 ? 1 : 0
+    turn_order until game_over?
+  end
 
-      get_player_input(n)
-      print_board
-    end
+  def turn_order
+    player_input(@current_player)
+    switch_player
+    print_board
+  end
+
+  def switch_player
+    @current_player = @current_player == @player1 ? @player2 : @player1
   end
 
   def place_mark(player, cell)
-    # return false unless @grid[cell]
-    if free_cell?(cell)
-      @grid[cell] = player.marker
-    else
-      false
-    end
+    @grid[cell] = player.marker
   end
 
-  private
-
-  def get_player_input(num)
-    puts "#{@players[num].marker} choose:"
-    while choice = @players[num].select_position
-      if free_cell?(choice)
-        place_mark(@players[num], choice)
+  def player_input(player)
+    puts "#{player.marker} choose:"
+    loop do
+      choice = player.select_position
+      if valid_cell?(choice)
+        place_mark(player, choice)
         break
       end
     end
@@ -58,14 +59,20 @@ class TicTacToe
     false
   end
 
-  def full_line?
+  def full_line?(grid = @grid)
     LINES.each do |line|
-      result = line.map { |i| @grid[i] }.uniq
+      result = line.map { |i| grid[i] }.uniq
 
       return result.first if result.count == 1 && result[0]
     end
     false
   end
+
+  def valid_cell?(cell)
+    cell.between?(1, 9) && @grid[cell].nil?
+  end
+
+  private
 
   def print_board
     puts " #{cell(1)} | #{cell(2)} | #{cell(3)}\n" \
@@ -77,9 +84,5 @@ class TicTacToe
 
   def cell(cell)
     @grid[cell] || ' '
-  end
-
-  def free_cell?(cell)
-    cell.between?(1, 9) && @grid[cell].nil?
   end
 end
